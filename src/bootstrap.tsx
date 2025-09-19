ae8a283a0bfe08c11c2f9cdae70599dab4ef2903
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2025-09-19 22:29:50 +0530

    fix: bootstrap issue

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,37 +1,38 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import Counter from './PracticeProblems/Counter';
+// import Counter from './PracticeProblems/Counter';
 
-import HookComponent from './PracticeProblems/HookComponent ';
-import Redux from './PracticeProblems/Redux';
-import Routers from './PracticeProblems/Routers';
+// import HookComponent from './PracticeProblems/HookComponent ';
+// import { ReduxApp } from './PracticeProblems/Redux';
+// import Routers from './PracticeProblems/Routers';
 
-import HOC from './PracticeProblems/HoC';
-import { Context, Provider } from './PracticeProblems/Context';
+// import HOC from './PracticeProblems/HoC';
+// import { Context, Provider } from './PracticeProblems/Context';
+// import FormHandle from './PracticeProblems/FormHandle';
 
-export const Root = () => {
-    return(
-        <>
-        <HOC isLogin={true} />
-        <br /><br />
+// export const Root = () => {
+//     return(
+//         <>
+//         <HOC isLogin={true} />
+//         <br /><br />
 
-        <Provider><Context/></Provider>
+//         <Provider><Context/></Provider>
         
-        <Counter/>
-        <br /><br />
+//         <Counter/>
+//         <br /><br />
         
         
-        <HookComponent/>
-        <br /><br />
+//         <HookComponent/>
+//         <br /><br />
 
-        <Redux/>
-        <br /><br />
+//         <ReduxApp/>
+//         <br /><br />
         
-        <Routers/>
-        <br /><br />
-        </>
-    );
-}
+//         <Routers/>
+//         <br /><br />
+//         </>
+//     );
+// }
 
 
 const container = document.getElementById('root');
